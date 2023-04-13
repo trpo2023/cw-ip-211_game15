7811deb082f7056f2a058971ae1a245ec2169eb8
albert22o <124139885+albert22o@users.noreply.github.com>
2023-04-13 11:52:56 +0700

    Source code for Grid is completed

--- src/main.cpp
+++ src/main.cpp
@@ -38,4 +38,38 @@ public:
                 rigth_index_blocks_[i][j] = i + j * n_;
             }
         }
-        
+        // Инициализируем блоки поля
+        blocks_ = new sf::RectangleShape* [n_];
+        texts_ = new sf::Text * [n_];
+        for (int i = 0; i < n_; ++i) {
+            blocks_[i] = new sf::RectangleShape[n_];
+            texts_[i] = new sf::Text[n_];
+            for (int j = 0; j < n_; ++j) {
+                if (index_blocks_[i][j]==0) {
+                    blocks_[i][j].setSize(sf::Vector2f(blockSize_, blockSize_));
+                    blocks_[i][j].setOutlineThickness(1.f);
+                    blocks_[i][j].setOutlineColor(sf::Color::Black);
+                    blocks_[i][j].setFillColor(sf::Color::Black);// Инициализируем номер блока
+                    sf::Vector2f pos(i * blockSize_, j * blockSize_);
+                    sf::Text text(std::to_string(index_blocks_[i][j]), font_, 70);
+                    text.setPosition(pos.x + 2.f, pos.y + 2.f);
+                    text.setFillColor(sf::Color::Black);
+                    texts_[i][j] = text;
+                    continue;
+                }
+                blocks_[i][j].setSize(sf::Vector2f(blockSize_, blockSize_));
+                blocks_[i][j].setOutlineThickness(1.f);
+                blocks_[i][j].setOutlineColor(sf::Color::Black);
+                blocks_[i][j].setFillColor(sf::Color::White);
+
+                // Инициализируем номер блока
+                sf::Vector2f pos(i * blockSize_, j * blockSize_);
+                sf::Text text(std::to_string(index_blocks_[i][j]), font_, 70);
+                text.setPosition(pos.x + 2.f, pos.y + 2.f);
+                text.setFillColor(sf::Color::Black);
+                texts_[i][j] = text;
+            }
+        }
+    }
+
+    
